443ed3f13f4bbbdc403279bbb946d50655dffdff
Nikita Ananiev <nickita.ananiev@yandex.ru>
2019-02-16 16:19:10 +0300

    Add multiple expression entering

--- src/main.py
+++ src/main.py
@@ -3,4 +3,16 @@ import rpn
 import lexeme
 
 #print(run.run(rpn.buildRPN(lexeme.lexemes('   1001 * sqrj(  9) '))))
-print(run.run(rpn.buildRPN(lexeme.lexemes(input()))))
+#print(run.run(rpn.buildRPN(lexeme.lexemes(input()))))
+
+while True:
+    expression = input('Please type an expression (or press ENTER for exit): ')
+    if len(expression) == 0:
+        break
+    lexemes = lexeme.lexemes(expression)
+    print(lexemes)
+    rpnLexemes = rpn.buildRPN(lexemes)
+    print(rpnLexemes)
+    result = run.run(rpnLexemes)
+    print(result)
+
