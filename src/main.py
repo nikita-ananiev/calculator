98ed90a569c313b1000836cf67b5323ca0ce90c3
Nikita Ananiev <nickita-ananiev@yandex.ru>
2019-05-06 15:14:55 +0300

    Fix % priority

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import run
 import rpn
 import lexeme
+import test
 
-#print(run.run(rpn.buildRPN(lexeme.lexemes('   1001 * sqrj(  9) '))))
-print(run.run(rpn.buildRPN(lexeme.lexemes(input()))))
-
+# print(run.run(rpn.buildRPN(lexeme.lexemes('   1001 * sqrj(  9) '))))
+# print(run.run(rpn.buildRPN(lexeme.lexemes(input()))))
+test.my_test()
 #while True:
     #expression = input('Please type an expression (or press ENTER for exit): ')
     #if len(expression) == 0:
